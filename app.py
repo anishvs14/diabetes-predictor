cf30e2ecd5d2a66cc2a8ff69e0d2d88eccab2df7
Anish Sirdeshpande <anish.sirdeshpande@gmail.com>
2025-04-22 01:45:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import numpy as np
 import joblib
-st.set_page_config(page_title="Health Risk Predictor", layout="wide", initial_sidebar_state="expanded")
 
 # Sidebar to choose prediction model
 st.sidebar.title("Choose Prediction Model")
@@ -49,13 +48,22 @@ else:
     st.markdown("Enter your heart health parameters to check your risk of heart disease:")
 
     age = st.number_input("Age", min_value=0, step=1)
+    sex = st.selectbox("Sex (0 = Female, 1 = Male)", [0, 1])
+    cp = st.selectbox("Chest Pain Type (0-3)", [0, 1, 2, 3])
     trestbps = st.number_input("Resting Blood Pressure")
     chol = st.number_input("Cholesterol Level")
+    fbs = st.selectbox("Fasting Blood Sugar > 120 mg/dl", [0, 1])
+    restecg = st.selectbox("Resting ECG Results (0-2)", [0, 1, 2])
     thalach = st.number_input("Max Heart Rate Achieved")
+    exang = st.selectbox("Exercise Induced Angina", [0, 1])
     oldpeak = st.number_input("ST Depression (Oldpeak)")
+    slope = st.selectbox("Slope of the peak exercise ST segment (0-2)", [0, 1, 2])
+    ca = st.selectbox("Number of major vessels (0-3)", [0, 1, 2, 3])
+    thal = st.selectbox("Thalassemia (0 = normal, 1 = fixed defect, 2 = reversible defect)", [0, 1, 2])
 
     if st.button("Predict"):
-        input_data = np.array([[age, trestbps, chol, thalach, oldpeak]])
+        input_data = np.array([[age, sex, cp, trestbps, chol, fbs, restecg,
+                                thalach, exang, oldpeak, slope, ca, thal]])
         scaled_input = heart_scaler.transform(input_data)
         prediction = heart_model.predict(scaled_input)[0]
 
